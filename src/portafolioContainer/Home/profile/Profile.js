dd551298145080f889f6155cb9463ebf32c27482
Moril <jeanmo333@gmail.com>
2022-12-26 20:38:12 -0300

    commit

--- src/portafolioContainer/Home/profile/Profile.js
+++ src/portafolioContainer/Home/profile/Profile.js
@@ -1,62 +1,68 @@
 import React from "react";
-import Typical from "react-typical";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
 import { motion } from "framer-motion";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./Profile.css";
 
-
-
-export default function Profile(props) {
+export default function Profile() {
+  const [text, count] = useTypewriter({
+    words: ["Estudiante 📚", "Autodidacta 💻", "Web Developer 🌐"],
+    loop: true,
+    delaySpeed: 2000,
+  });
 
   let history = useHistory();
 
-  const contratame = () =>{
-    history.push('/contact');
-  }
+  const contratame = () => {
+    history.push("/contact");
+  };
 
   return (
     <div className="profile-container">
       <div className="profile-parent">
-        <motion.div 
-         initial={{
+        <motion.div
+          initial={{
             x: -500,
             opacity: 0,
-           // scale: 0.5
+            // scale: 0.5
           }}
           animate={{
             x: 0,
-            opacity:1,
-           // scale: 1
+            opacity: 1,
+            // scale: 1
           }}
           transition={{
-            duration: 1.5
+            duration: 1.5,
           }}
-        className="profile-details">
+          className="profile-details"
+        >
           <div className="colz">
-                <div className="colz-icon">
-                    <a href="https://www.facebook.com/profile.php?id=100049311472102"
-                     target="_blank"
-                    >
-                        <i className="fa fa-facebook-square"></i>
-                    </a>
+            <div className="colz-icon">
+              <a
+                href="https://www.facebook.com/profile.php?id=100049311472102"
+                target="_blank"
+              >
+                <i className="fa fa-facebook-square"></i>
+              </a>
 
+              <a
+                href="https://www.instagram.com/moril_jean_francois/?hl=es-la"
+                target="_blank"
+              >
+                <i className="fa fa-instagram"></i>
+              </a>
 
-                    <a href="https://www.instagram.com/moril_jean_francois/?hl=es-la"
-                     target="_blank"
-                    >
-                        <i className="fa fa-instagram"></i>
-                    </a>
+              <a
+                href="https://www.youtube.com/channel/UC0Mzt5rTeDr9HYqAwg6rxXg"
+                target="_blank"
+              >
+                <i className="fa fa-youtube-square"></i>
+              </a>
 
-                    <a href="https://www.youtube.com/channel/UC0Mzt5rTeDr9HYqAwg6rxXg" 
-                     target="_blank"
-                    >
-                        <i className="fa fa-youtube-square"></i>
-                    </a>
-
-                    <a to="#">
-                        <i className="fa fa-twitter"></i>
-                    </a>
-                </div>
+              <a to="#">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
           </div>
 
           <div className="profile-details-name">
@@ -71,55 +77,43 @@ export default function Profile(props) {
             <span className="primary-text">
               {""}
               <h1>
-                {""}
-
-                <Typical
-                  loop={Infinity}
-                  steps={[
-                    "Estudiante 📚",
-                    2000,
-                    "Autodidacta 💻",
-                    2000,
-                    "Web Developer 🌐",
-                    1000,
-                  ]}
-                />
+                <span className="mr-3">{text}</span>
+                <Cursor cursorColor="#FF5823" />
               </h1>
 
               <span className="profile-role-tagline">
-                Habilidad para crear aplicaciones con operaciones front-end y back-end.
+                Habilidad para crear aplicaciones con operaciones front-end y
+                back-end.
               </span>
             </span>
           </div>
 
           <div className="profile-options">
-            <button className="btn primary-btn" onClick={contratame} >
+            <button className="btn primary-btn" onClick={contratame}>
               {""}
               Contratame {""}
             </button>
 
-            <a
-              href="Cv-Moril-jean.pdf"
-              download="Cv-Moril-jean.pdf"
-            >
+            <a href="Cv-Moril-jean.pdf" download="Cv-Moril-jean.pdf">
               <button className="btn highlighted-btn">Descargar CV</button>
             </a>
           </div>
         </motion.div>
 
-        <motion.div 
-       initial={{
+        <motion.div
+          initial={{
             x: 500,
-            opacity: 0
+            opacity: 0,
           }}
           animate={{
             x: 0,
-            opacity:1
+            opacity: 1,
           }}
           transition={{
-            duration: 1.5
+            duration: 1.5,
           }}
-        className="profile-picture">
+          className="profile-picture"
+        >
           <div className="profile-picture-background"></div>
         </motion.div>
       </div>
